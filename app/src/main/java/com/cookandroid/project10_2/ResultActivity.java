69d35822d8c2a6a7d04daead095cd14491d07d56
seojin-06 <seojin7376@khu.ac.kr>
2023-10-10 13:54:48 +0900

    commit

---
+++ app/src/main/java/com/cookandroid/project10_2/ResultActivity.java
@@ -0,0 +1,76 @@
+package com.cookandroid.project10_2;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+        setTitle("투표 결과");
+
+        ImageView resultView = (ImageView) findViewById(R.id.resultView);
+        TextView resultText = (TextView) findViewById(R.id.resultText);
+
+        Intent intent = getIntent();
+        int[] voteResult = intent.getIntArrayExtra("VoteCount");
+        String[] imageName = intent.getStringArrayExtra("ImageName");
+
+        TextView tv[] = new TextView[imageName.length];
+        RatingBar rbar[] = new RatingBar[imageName.length];
+
+        Integer imageFileId[] = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3,
+                R.drawable.pic4, R.drawable.pic5, R.drawable.pic6,
+                R.drawable.pic7, R.drawable.pic8, R.drawable.pic9};
+        Integer tvID[] = { R.id.tv1, R.id.tv2, R.id.tv3, R.id.tv4, R.id.tv5,
+                R.id.tv6, R.id.tv7, R.id.tv8, R.id.tv9 };
+        Integer rbarID[] = { R.id.rbar1, R.id.rbar2, R.id.rbar3, R.id.rbar4,
+                R.id.rbar5, R.id.rbar6, R.id.rbar7, R.id.rbar8, R.id.rbar9 };
+
+
+        for (int i = 0; i < voteResult.length; i++) {
+            tv[i] = (TextView) findViewById(tvID[i]);
+            rbar[i] = (RatingBar) findViewById(rbarID[i]);
+        }
+
+        for (int i = 0; i < voteResult.length; i++) {
+            tv[i].setText((CharSequence) imageName[i]);
+            rbar[i].setRating((float) voteResult[i]);
+        }
+
+        for (int i = 0; i < voteResult.length; i++) {
+            int max = 0;
+            int maxResult = 0;
+            if (voteResult[i] > max) {
+                max = voteResult[i];
+                maxResult = i;
+                resultText.setText(imageName[maxResult]);
+                resultView.setImageResource(imageFileId[maxResult]);
+            } else {
+                i++;
+            }
+        }
+
+        Button btnReturn = (Button) findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
